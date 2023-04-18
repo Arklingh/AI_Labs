21fb1f295cfbd3a67501fbbe5f4ae4d4cb06531e
Arklingh <andriikurochka@i.ua>
2023-04-18 12:18:47 +0300

    Initial commit

--- index.py
+++ index.py
@@ -6,13 +6,13 @@ PRECISION = 99.9
 
 RankedSolution = (float, [float])
 
-def func(x: int, y: int, z: int) -> int:
+def math_func(x: int, y: int, z: int) -> int:
     return 1 / (1 + (x-2)**2 + (y+1)**2 + (z-1)**2)
 
-def fitness(x: int, y: int, z: int) -> int:
-    return func(x, y, z) * 100
+def fit(x: int, y: int, z: int) -> int:
+    return math_func(x, y, z) * 100
 
-def generate_population(size: int) -> [float]:
+def gen_pop(size: int) -> [float]:
     solutions = []
     for s in range(size):
         solutions.append((rng.uniform(-1000, 1000),
@@ -24,14 +24,14 @@ def generate_population(size: int) -> [float]:
 def rank_solutions(solutions: [float]) -> RankedSolution:
     ranked_solutions = []
     for s in solutions:
-        ranked_solutions.append((fitness(s[0], s[1], s[2]), s))
+        ranked_solutions.append((fit(s[0], s[1], s[2]), s))
     return ranked_solutions
 
-def set_new_gen(bestsolutions: [float]) -> [float]:
+def set_new_gen(best_solutions: [float]) -> [float]:
     elements_x = []
     elements_y = []
     elements_z = []
-    for s in bestsolutions:
+    for s in best_solutions:
         elements_x.append(s[1][0])
         elements_y.append(s[1][1])
         elements_z.append(s[1][2])
@@ -44,26 +44,26 @@ def set_new_gen(bestsolutions: [float]) -> [float]:
         new_gen.append((el1, el2, el3))
     return new_gen
 
-def run_evolution(generation_limit: int, precision: float):
-    solutions = generate_population(SIZE)
+def run_evol(generation_limit: int, precision: float):
+    solutions = gen_pop(SIZE)
 
     for i in range(generation_limit):
         ranked_solutions = rank_solutions(solutions)
         ranked_solutions.sort()
         ranked_solutions.reverse()
         
-        print(f"Gen {i} best solution")
+        print(f"{i}'s generation best solution:")
         print(ranked_solutions[0])
 
         if ranked_solutions[0][0] > precision:
             break
 
-        bestsolutions = ranked_solutions[:100]
+        best_solutions = ranked_solutions[:100]
         
-        solutions = set_new_gen(bestsolutions)
+        solutions = set_new_gen(best_solutions)
 
 def main():
-    run_evolution(GENERATION_LIMIT, PRECISION)
+    run_evol(GENERATION_LIMIT, PRECISION)
 
 if __name__ == "__main__":
     main()
